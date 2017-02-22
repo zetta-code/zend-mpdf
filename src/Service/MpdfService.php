3de841336d052c99a16e7cdb0c1aed82472ae7f8
Thiago S. Santos <thiagos.xsantos@gmail.com>
2017-02-22 09:48:13 -0300

    Alteração na anotation doc

--- src/Service/MpdfService.php
+++ src/Service/MpdfService.php
@@ -7,9 +7,6 @@
 namespace Zetta\ZendMPDF\Service;
 
 use Interop\Container\ContainerInterface;
-use Interop\Container\Exception\ContainerException;
-use Zend\ServiceManager\Exception\ServiceNotCreatedException;
-use Zend\ServiceManager\Exception\ServiceNotFoundException;
 use Zend\ServiceManager\Factory\FactoryInterface;
 use mPDF;
 
@@ -22,16 +19,7 @@ class MpdfService implements FactoryInterface
     const JPGRAPH_DIR = self::MPDF_DIR . 'jpgraph/';
 
     /**
-     * Create an object
-     *
-     * @param  ContainerInterface $container
-     * @param  string $requestedName
-     * @param  null|array $options
-     * @return object
-     * @throws ServiceNotFoundException if unable to resolve the service.
-     * @throws ServiceNotCreatedException if an exception is raised when
-     *     creating a service.
-     * @throws ContainerException if any other error occurs
+     * {@inheritdoc}
      */
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
